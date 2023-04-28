9d40a4b0e9765a426913cf4ec50af0e5db1d4709
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-04-28 15:13:22 +0300

    add site form/page

--- pages/Sites.js
+++ pages/Sites.js
@@ -1,5 +1,12 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Sites() {
-  return <Layout> Sites</Layout>;
+  return (
+    <Layout>
+      <Link className="linkAddButton" href={"/Sites/new"}>
+        Add a new site
+      </Link>
+    </Layout>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
-  return <Layout> test</Layout>;
+  const { data: session } = useSession();
+  if (!session) return;
+  return (
+    <Layout>
+      <div className="flex text-gray-900  bg-neutral-300  rounded-lg p-4 justify-between">
+        <h2 className="my-auto">
+          Hi, <b>{session?.user?.name} </b>!
+        </h2>
+
+        <div className="flex bg-slate-100 pr-2 rounded-md shadow-xl overflow-hidden gap-2">
+          <img
+            src={session?.user?.image}
+            alt="goole profile img"
+            className="w-6 h-6"
+          />
+          {session?.user?.email}
+        </div>
+      </div>
+    </Layout>
+  );
 }
