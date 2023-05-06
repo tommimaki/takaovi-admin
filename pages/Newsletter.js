b436899f374f195026aa3ca9d605cd75646d6de0
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-05-06 14:23:58 +0200

    newsletter subscribers and functionalities added

--- pages/Newsletter.js
+++ pages/Newsletter.js
@@ -3,15 +3,34 @@ import axios from "axios";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
-export default function Sites() {
+export default function Newsletters() {
   const [letters, setLetters] = useState([]);
   useEffect(() => {
-    axios.get("/api/newsletter").then((response) => {
-      setLetters(response.data);
-    });
+    axios
+      .get(`${process.env.NEXT_PUBLIC_BACKEND_API_BASE_URL}/api/newsletter`)
+      .then((response) => {
+        setLetters(response.data);
+      });
   }, []);
+
+  const copyEmails = () => {
+    const emailList = letters.map((letter) => letter.email).join(", ");
+    navigator.clipboard.writeText(emailList).then(
+      () => {
+        alert("Email addresses copied to clipboard");
+      },
+      (err) => {
+        console.error("Could not copy email addresses: ", err);
+      }
+    );
+  };
+
   return (
     <Layout>
+      <h1 className="text-center mb-4">Newsletter</h1>
+      <button onClick={copyEmails} className="btnGreen">
+        Copy All Addresses
+      </button>
       <table className="basic mt-4">
         <thead>
           <tr>
@@ -21,29 +40,14 @@ export default function Sites() {
         </thead>
         <tbody>
           {letters.map((letter) => (
-            <tr key={letter.title}>
-              <td>{letter.title}</td>
+            <tr key={letter._id}>
+              <td>{letter.email}</td>
               <td>
                 <div className="flex gap-4">
-                  <Link className="btnGreen" href={"/Sites/edit/" + site._id}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                      />
-                    </svg>
-                    Edit
-                  </Link>
-
-                  <Link className="btnRed" href={"/Letter/delete/" + site._id}>
+                  <Link
+                    className="btnRed"
+                    href={"/Newsletters/delete/" + letter._id}
+                  >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
